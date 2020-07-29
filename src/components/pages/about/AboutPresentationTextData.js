72f4b2f5786a649f7a13f0354647fbd23072788a
tadas <ttadas10@gmail.com>
2020-07-29 12:13:49 +0300

    Beta version release

--- src/components/pages/about/AboutPresentationTextData.js
+++ src/components/pages/about/AboutPresentationTextData.js
@@ -5,25 +5,25 @@ export const textData =
         {
             id: 1,
             name: 'about',
-            text: '2014 m. keturi draugai iš savo motinos gavo pakankamai investicijų, kad atidarytų mažą valgyklą Lietuvos mieste Vilniuje. Valgyklėle buvo pavadinta "ItaPizza". PPavadinimo kilmė turėjo reikšmę, kuri buvo atidaryti Itališko maisto valgyklą Lietuvoje. Po kelerių metų, valgyklėle tapo organizacija, kuri dabar Lietuvoje turi daugiau nei 14 punktų, kuriuose galima tiek pavalgyti tiek užsisakyti išsinešimui.',
+            text: '2014 m. keturi draugai iš savo motinos gavo pakankamai investicijų, kad atidarytų mažą valgyklą Lietuvos mieste Vilniuje. Valgyklėle buvo pavadinta "ItaPizza". Pavadinimo kilmė turėjo reikšmę, kuri buvo atidaryti Itališko maisto valgyklėle Lietuvoje. Po kelerių metų, valgyklėle tapo organizacija, kuri dabar Lietuvoje turi daugiau nei 14 punktų, kuriuose galima tiek pavalgyti tiek užsisakyti išsinešimui.',
             img: 'https://www.britmums.com/wp-content/uploads/2018/06/food-800x400.jpg'
         },
         {
             id: 2,
             name: 'missions',
-            text: 'Mūsų organizacijos užduotis yra patenkinti kiekvieno kliento skonio poreikį. Klientams kilus idėjų dėl maisto variacijos, kuo greičiau stengiamės išpilditi kliento pageidavimus. Klientai yra mums dovana, kurią branginamę ir gerbiamę. Kiekvienas klientas privalo būti maloniai aptarnautas ir jeigu klientas turi kokių pageidavimų, mes steigiames juos išpildytį.',
+            text: 'Mūsų organizacijos užduotis yra patenkinti kiekvieno kliento skonio poreikį. Klientams kilus idėjų dėl maisto variacijos, kuo greičiau stengiamės išpildyti kliento pageidavimus. Klientai yra mums dovana, kurią branginame ir gerbiame. Kiekvienas klientas privalo būti maloniai aptarnautas ir jeigu klientas turi kokių pageidavimų, mes stengiames juos išpildyti.',
             img: 'https://images.squarespace-cdn.com/content/v1/596fbe772e69cf17df3be38d/1571973000555-P63QVETSBA02Q064JT8D/ke17ZwdGBToddI8pDm48kGVAqUhYrGdhEGudzFls7K1Zw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PIJit7nLBfbiTxuteTYcG_gtVxz0kc0mKQ8mrhuae_pfEKMshLAGzx4R3EDFOm1kBS/couplescooking.jpg'
         },
         {
             id: 3,
             name: 'goals',
-            text: 'Nuo pat pirmos akimirkos, keturi draugai galėjo pažvelgti į savo klientus, nesiblaškydami ir garantuodami jiems geriausią maistą iš vietinių valgyklos darbuotojų, atsižvelgiant į tai, kad draugai žinojo geriausius ūkio specialistus, kurie suteigdavo kokybiškiausius ir aukščiausios rūšies produktus. Nuo to laiko mūsų žemės ūkio specialistai vystė produktus tiesiogiai Lietuvoje. Jokia kita organizacija negerbia maisto taip kaip "ItaPizza".',
+            text: 'Nuo pat pirmos akimirkos, keturi draugai galėjo pažvelgti į savo klientus, nesiblaškydami ir garantuodami jiems geriausią maistą iš vietinių valgyklos darbuotojų, atsižvelgiant į tai, kad draugai žinojo geriausius ūkio specialistus, kurie suteikdavo kokybiškiausius ir aukščiausios rūšies produktus. Nuo to laiko mūsų žemės ūkio specialistai vystė produktus tiesiogiai Lietuvoje. Jokia kita organizacija negerbia maisto taip kaip "ItaPizza".',
             img: 'https://www.britmums.com/wp-content/uploads/2019/06/BBQ-800x400.jpg'
         },
         {
             id: 4,
             name: 'values',
-            text: 'Mūsų organizacijos užduotis yra patenkinti kiekvieno kliento skonio poreikį. Klientams kilus idėjų dėl maisto variacijos, kuo greičiau stengiamės išpilditi kliento pageidavimus. Klientai yra mums dovana, kurią branginamę ir gerbiamę. Kiekvienas klientas privalo būti maloniai aptarnautas ir jeigu klientas turi kokių pageidavimų, mes steigiames juos išpildytį.',
+            text: 'Mūsų organizacijos užduotis yra patenkinti kiekvieno kliento skonio poreikį. Klientams kilus idėjų dėl maisto variacijos, kuo greičiau stengiamės išpildyti kliento pageidavimus. Klientai yra mums dovana, kurią branginame ir gerbiame. Kiekvienas klientas privalo būti maloniai aptarnautas ir jeigu klientas turi kokių pageidavimų, mes stengiames juos išpildyti.',
             img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRguS_SXvrZiO2h0Nbf4brc6aC75XlnzRoA-w&usqp=CAU'
         }
 ]
