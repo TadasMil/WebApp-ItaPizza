3a55a87b26927cb03bf1b0cf0f2363834a96e4e9
tadas <ttadas10@gmail.com>
2020-07-29 11:52:35 +0300

    Fixed some common bugs

--- src/components/pages/about/AboutPresentationTextData.js
+++ src/components/pages/about/AboutPresentationTextData.js
@@ -5,25 +5,25 @@ export const textData =
         {
             id: 1,
             name: 'about',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            text: '2014 m. keturi draugai iš savo motinos gavo pakankamai investicijų, kad atidarytų mažą valgyklą Lietuvos mieste Vilniuje. Valgyklėle buvo pavadinta "ItaPizza". PPavadinimo kilmė turėjo reikšmę, kuri buvo atidaryti Itališko maisto valgyklą Lietuvoje. Po kelerių metų, valgyklėle tapo organizacija, kuri dabar Lietuvoje turi daugiau nei 14 punktų, kuriuose galima tiek pavalgyti tiek užsisakyti išsinešimui.',
             img: 'https://www.britmums.com/wp-content/uploads/2018/06/food-800x400.jpg'
         },
         {
             id: 2,
             name: 'missions',
-            text: 'Albus, dexter particulas grauiter consumere de ferox, bi-color abactus. Impositios studere, tanquam mirabilis hippotoxota. Cur torus manducare? Pol, vox! Cum barcas nocere, omnes specieses contactus Lotus advenas ducunt ad gemna. Ubi est domesticus domina? Heu, barbatus mens! Cum elogium favere, omnes lubaes tractare talis, barbatus adiuratores. Mirabilis hydras ducunt ad danista. Dominas sunt accentors de germanus cacula. Amicitias prarere in alta muta! Ecce, bubo! Nunquam promissio verpa. Talis, primus fugas recte consumere de audax, festus indictio. Nunquam quaestio scutum. Valebats',
+            text: 'Mūsų organizacijos užduotis yra patenkinti kiekvieno kliento skonio poreikį. Klientams kilus idėjų dėl maisto variacijos, kuo greičiau stengiamės išpilditi kliento pageidavimus. Klientai yra mums dovana, kurią branginamę ir gerbiamę. Kiekvienas klientas privalo būti maloniai aptarnautas ir jeigu klientas turi kokių pageidavimų, mes steigiames juos išpildytį.',
             img: 'https://images.squarespace-cdn.com/content/v1/596fbe772e69cf17df3be38d/1571973000555-P63QVETSBA02Q064JT8D/ke17ZwdGBToddI8pDm48kGVAqUhYrGdhEGudzFls7K1Zw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PIJit7nLBfbiTxuteTYcG_gtVxz0kc0mKQ8mrhuae_pfEKMshLAGzx4R3EDFOm1kBS/couplescooking.jpg'
         },
         {
             id: 3,
             name: 'goals',
-            text: 'Albus, dexter particulas grauiter consumere de ferox, bi-color abactus. Impositios studere, tanquam mirabilis hippotoxota. Cur torus manducare? Pol, vox! Cum barcas nocere, omnes specieses contactus Lotus advenas ducunt ad gemna. Ubi est domesticus domina? Heu, barbatus mens! Cum elogium favere, omnes lubaes tractare talis, barbatus adiuratores. Mirabilis hydras ducunt ad danista. Dominas sunt accentors de germanus cacula. Amicitias prarere in alta muta! Ecce, bubo! Nunquam promissio verpa. Talis, primus fugas recte consumere de audax, festus indictio. Nunquam quaestio scutum. Valebats',
+            text: 'Nuo pat pirmos akimirkos, keturi draugai galėjo pažvelgti į savo klientus, nesiblaškydami ir garantuodami jiems geriausią maistą iš vietinių valgyklos darbuotojų, atsižvelgiant į tai, kad draugai žinojo geriausius ūkio specialistus, kurie suteigdavo kokybiškiausius ir aukščiausios rūšies produktus. Nuo to laiko mūsų žemės ūkio specialistai vystė produktus tiesiogiai Lietuvoje. Jokia kita organizacija negerbia maisto taip kaip "ItaPizza".',
             img: 'https://www.britmums.com/wp-content/uploads/2019/06/BBQ-800x400.jpg'
         },
         {
             id: 4,
             name: 'values',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            text: 'Mūsų organizacijos užduotis yra patenkinti kiekvieno kliento skonio poreikį. Klientams kilus idėjų dėl maisto variacijos, kuo greičiau stengiamės išpilditi kliento pageidavimus. Klientai yra mums dovana, kurią branginamę ir gerbiamę. Kiekvienas klientas privalo būti maloniai aptarnautas ir jeigu klientas turi kokių pageidavimų, mes steigiames juos išpildytį.',
             img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRguS_SXvrZiO2h0Nbf4brc6aC75XlnzRoA-w&usqp=CAU'
         }
 ]
